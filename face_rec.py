f92bd206a514657694e17e3716e6312e532eb823
Robert Pham <robqpham@gmail.com>
2015-10-25 12:43:24 -0700

    added svm and pica

--- face_rec.py
+++ face_rec.py
@@ -1,41 +1,141 @@
 import numpy as np
 import cv2
-import sklearn
+import lbp
+from sklearn import decomposition
+import pickle
+import sys
+from sklearn import svm
+# import matplotlib.pyplot as plt
+# from mpl_toolkits.mplot3d import Axes3D
 
 IMAGE_PATH = '../datasets/yalefaces_png/'
-LIST_NAME = 'subjects.lst'
-
-clf = sklearn.svm.LinearSVC()
-features = 0
-y = 0
+TRAIN_LIST = 'subjects_train.lst'
+TEST_LIST = 'subjects_test.lst'
 
 
-def circular_lbp(img):
-    pass
+class FaceRec:
 
-# Get the list of images
-img_list = []
-with open(IMAGE_PATH + LIST_NAME, 'r') as f:
-        while True:
-            filename = f.readline()
-            # Zero length indicates EOF
-            if len(filename) == 0:
-                break
-            filename = filename.rstrip()
-            # get filename without return
-            img_list.append(IMAGE_PATH + filename)
+    # Classifier
+    clf = svm.LinearSVC()  # one versus rest train only one class
 
-# Read each image for training
-for img_name in img_list:
-    img = cv2.imread(img_name, cv2.IMREAD_GRAYSCALE)
-    cv2.imshow('image', img)
-    cv2.waitKey(0)
+    # PCA
+    pca = decomposition.PCA(n_components=3)
 
-    # Calculate the lbp histograms
-    features = circular_lbp(img)
+    # Features
+    features = 0
+    targets = 0
 
-    # Train the Classifier
-clf.fit(features, y)
+    def get_pca_features(self):
+        # Get the list of images
+        img_list = []
+        y = []
+        with open(IMAGE_PATH + TRAIN_LIST, 'r') as f:
+                while True:
+                    filename = f.readline()
+                    # Zero length indicates EOF
+                    if len(filename) == 0:
+                        break
+                    filename = filename.rstrip()
+                    target = filename.split('.')
+                    y.append(target[0])
 
-# Read images for testing
-# Test the classifier
+                    # get filename without return
+                    img_list.append(IMAGE_PATH + 'train/' + filename)
+
+        X = []
+        # Read each image for PCA
+        for img_name in img_list[:10]:
+            img = cv2.imread(img_name, cv2.IMREAD_GRAYSCALE)
+            cv2.imshow('image', img)
+
+            # Calculate the lbp histograms
+            features = lbp.circular_lbp(img)
+            cv2.imshow('LBP', np.int8(features))
+
+            # separate the image into grids and get histograms
+            block_size = (3, 3)
+            sizex, sizey = features.shape
+
+            for i in range(0, sizex, block_size[0]):
+                for j in range(0, sizey, block_size[1]):
+                    X.append(np.bincount(
+                        features[i:i+block_size[0], j:j+block_size[1]].ravel(),
+                        minlength=256))
+
+        print np.array(X).shape
+        self.features = np.array(X)
+        print self.X
+        # Train the Classifier
+        self.pca.fit(np.array(X))
+
+    def train(self, X=features, y=targets):
+        X = np.asarray(X)
+        print X
+        y = np.asarray(y)
+        self.clf.fit(X, y)
+
+    def test(self, img):
+        features = lbp.circular_lbp(img)
+        X = self.pca.transfrom(features)
+        prediction = self.clf.predict(X.ravel())
+        return prediction
+
+
+# ==================================================
+#
+# Main
+#
+# ==================================================
+facerec = FaceRec()
+loading = False
+
+if(str(sys.argv[1]) == '-l'):
+    loading = True
+
+# PCA
+if loading:
+    print 'loaded pre-trained human detector'
+    with open('face_rec_pca.pkl', 'rb') as input:
+        facerec.pca = pickle.load(input)
+    with open('face_rec_X.pkl', 'rb') as input:
+        facerec.features = pickle.load(input)
+else:
+    facerec.get_pca_features()
+    with open('face_rec_pca.pkl', 'wb') as output:
+            pickle.dump(facerec.pca, output, pickle.HIGHEST_PROTOCOL)
+    with open('face_rec_X.pkl', 'wb') as output:
+        pickle.dump(facerec.features, output, pickle.HIGHEST_PROTOCOL)
+
+# Train the classifier
+facerec.train()
+
+# Load a test image
+
+print facerec.pca
+
+# Read images for testing=
+
+
+
+# fig = plt.figure(1, figsize=(4, 3))
+# plt.clf()
+# ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
+
+# ax.scatter(X[:, 0], X[:, 1], X[:, 2], cmap=plt.cm.spectral)
+
+# x_surf = [X[:, 0].min(), X[:, 0].max(),
+#           X[:, 0].min(), X[:, 0].max()]
+# y_surf = [X[:, 0].max(), X[:, 0].max(),
+#           X[:, 0].min(), X[:, 0].min()]
+# x_surf = np.array(x_surf)
+# y_surf = np.array(y_surf)
+# v0 = facerec.pca.transform(facerec.pca.components_[0])
+# v0 /= v0[-1]
+# v1 = facerec.pca.transform(facerec.pca.components_[1])
+# v1 /= v1[-1]
+
+# ax.w_xaxis.set_ticklabels([])
+# ax.w_yaxis.set_ticklabels([])
+# ax.w_zaxis.set_ticklabels([])
+
+# plt.show()
---
+++ lbp.py
@@ -0,0 +1,88 @@
+import numpy as np
+
+
+def circular_lbp(img, radius=1, neighbors=8):
+    '''Circular LBP implementation
+
+    Ref:
+    Philipp Wagner
+    https://github.com/bytefish/facerec/blob/master/py/facerec/lbp.py
+    '''
+    # make sure that you always have one
+    # neighbor and no more than 31
+    neighbors = max(min(neighbors, 31), 1)
+    rows, cols = img.shape
+
+    # get the circle
+    angles = 2 * np.pi / neighbors
+    theta = np.arange(0, 2*np.pi, angles)
+
+    # get sample point locations around center
+    sample_points = np.array([-np.sin(theta), np.cos(theta)]).T
+    sample_points *= radius
+
+    # Get bounding box of samples
+    miny = min(sample_points[:, 0])
+    maxy = max(sample_points[:, 0])
+    minx = min(sample_points[:, 1])
+    maxx = max(sample_points[:, 1])
+
+    # calculate block size, each LBP code is
+    # computed within a block of size bsizey*bsizex
+    blocksizey = np.ceil(max(maxy, 0)) - np.floor(min(miny, 0)) + 1
+    blocksizex = np.ceil(max(maxx, 0)) - np.floor(min(minx, 0)) + 1
+
+    # Staring point
+    origy = 0 - np.floor(min(miny, 0))
+    origx = 0 - np.floor(min(minx, 0))
+
+    # output image size
+    dx = cols - blocksizex + 1
+    dy = rows - blocksizey + 1
+
+    # Center points
+    C = np.asarray(img[origy:origy+dy, origx:origx+dx], dtype=np.uint8)
+
+    # image with LBPs
+    result = np.zeros((dy, dx), dtype=np.uint32)
+
+    # loop through every sample and
+    # compare with all center points in image
+    for i, p in enumerate(sample_points):
+        # get coordinate of sample points
+        y, x = p + (origy, origx)
+
+        # Calculate floors, ceils and rounds for the x and y.
+        # these are points used to interpolate
+        #
+        #  fx,fy|--------| cx,fy
+        #       | *(x,y) |
+        #       |        |
+        #  fx,cy|--------| cx,cy
+        #
+        fx = np.floor(x)
+        fy = np.floor(y)
+        cx = np.ceil(x)
+        cy = np.ceil(y)
+
+        # calculate fractional part if there is one
+        ty = y - fy
+        tx = x - fx
+
+        # calculate  bilinear interpolation weights
+        # for the sample point
+        w1 = (1 - tx) * (1 - ty)
+        w2 = tx * (1 - ty)
+        w3 = (1 - tx) * ty
+        w4 = tx * ty
+
+        # calculate interpolated image
+        N = w1 * img[fy:fy + dy, fx:fx + dx]    # top left
+        N += w2 * img[fy:fy + dy, cx:cx + dx]   # top right
+        N += w3 * img[cy:cy + dy, fx:fx + dx]   # bottom left
+        N += w4 * img[cy:cy + dy, cx:cx + dx]   # bottom right
+
+        # update LBP codes
+        D = N >= C
+        result += (1 << i) * D
+    return result
